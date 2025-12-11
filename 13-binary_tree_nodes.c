f056d410ab9bf8032c3204ec94292d625cab90e9
Hedygh <dyouahes@gmail.com>
2025-12-11 12:55:51 +0100

    fix betty 13

--- 13-binary_tree_nodes.c
+++ 13-binary_tree_nodes.c
@@ -4,14 +4,12 @@
  * binary_tree_nodes - Compte les nœuds avec au moins un enfant
  * @tree: Pointeur vers la racine de l’arbre
  *
- * Description: Un nœud est compté s’il a au moins un enfant (gauche ou droit).
- * Si tree est NULL, la fonction retourne 0.
+ * Description: Un nœud est compté s’il a au moins un enfant
  *
  * Return: Nombre de nœuds ayant au moins un enfant
  */
 size_t binary_tree_nodes(const binary_tree_t *tree)
 {
-	
 	if (tree == NULL)
 	return (0);
 
